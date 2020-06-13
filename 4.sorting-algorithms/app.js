2546edf58bc92e3310315550cfd0c532e9e0bd84
Marco Caldera <caldux@me.com>
2020-06-13 12:31:30 +0200

    Bubblesort

---
+++ 4.sorting-algorithms/app.js
@@ -0,0 +1,27 @@
+const bubbleSort = (arr) => {
+    const resultArray = [...arr]
+
+    for (let outer = 0; outer < resultArray.length; outer++) {
+        let outerEl = resultArray[outer]
+        for (let inner = outer + 1; inner < resultArray.length; inner++) {
+            let innerEl = resultArray[inner]
+            // [2, 1] => [1, 2]
+            if (outerEl > innerEl) { // cambiare < > per passare da ascending a descending order
+                resultArray[outer] = innerEl
+                resultArray[inner] = outerEl
+
+                outerEl = resultArray[outer]
+                innerEl = resultArray[inner]
+            }
+
+        }
+    }
+
+    return resultArray
+}
+
+
+
+const sortedArray = bubbleSort([5, 10, -3])
+// const sortedArray = bubbleSort([5, 10, -3, -10, 1, 100, 99])
+console.log(sortedArray)
