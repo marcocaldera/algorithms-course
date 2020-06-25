427bc668aa244611792ee4c979594a5f96da1f61
Marco Caldera <marco.caldera@icloud.com>
2020-06-25 21:58:45 +0200

    Improved knapsack

--- 7.complex-algo/app.js
+++ 7.complex-algo/app.js
@@ -1,16 +1,24 @@
-const knapsack = (items, cap, itemIndex) => {
-    if (cap === 0 || itemIndex < 0) {
-        return { items: [], value: 0, weight: 0 } // Empty knapsack 
-    }
-    if (cap < items[itemIndex].weight) {
-        return knapsack(items, cap, itemIndex - 1)
+const knapsackFn = (items, cap, itemIndex, memo) => {
+    console.log("RUNNING")
+    if (memo[cap][itemIndex]) {
+        console.log('Used memo')
+        return memo[cap][itemIndex]
     }
 
-    const sackWithItem = knapsack(items, cap - items[itemIndex].weight, itemIndex - 1)
-    const sackWithoutItem = knapsack(items, cap, itemIndex - 1)
+    if (cap === 0 || itemIndex < 0) {
+        return { items: [], value: 0, weight: 0 } // Empty knapsack
+    }
+    if (cap < items[itemIndex].weight) {
+        return knapsackFn(items, cap, itemIndex - 1, memo)
+    }
+
+    const sackWithItem = knapsackFn(items, cap - items[itemIndex].weight, itemIndex - 1, memo)
+    const sackWithoutItem = knapsackFn(items, cap, itemIndex - 1, memo)
 
     const valueWithItem = sackWithItem.value + items[itemIndex].value
     const valueWithoutItem = sackWithoutItem.value
+
+    let resultSack
 
     if (valueWithItem > valueWithoutItem) {
         const updatedSack = {
@@ -18,19 +26,36 @@ const knapsack = (items, cap, itemIndex) => {
             value: sackWithItem.value + items[itemIndex].value,
             weight: sackWithItem.weight + items[itemIndex].weight,
         }
-        return updatedSack
+        resultSack = updatedSack
     } else {
-        return sackWithoutItem
+        resultSack = sackWithoutItem
     }
 
+    memo[cap][itemIndex] = resultSack
+
+    return resultSack
+
+}
+
+function knapsack(items, cap, index) {
+    const mem = Array.from(Array(cap + 1), () =>
+        Array(items.length).fill(undefined)
+    );
+    // const mem = Array(cap + 1).fill(Array(items.length).fill(undefined))
+    console.log(mem)
+    return knapsackFn(items, cap, index, mem)
 }
 
 const items = [
     { name: 'a', value: 3, weight: 3 },
     { name: 'b', value: 6, weight: 8 },
-    { name: 'c', value: 10, weight: 3 }
+    { name: 'c', value: 10, weight: 3 },
+    // { name: 'd', value: 2, weight: 2 },
 ]
 const maxCap = 8
 
 const sack = knapsack(items, maxCap, items.length - 1)
 console.log(sack)
+
+// Time complexity without memoization: O(2^n)
+// Time complexity without memoization: O(n*C) (c is the capacity/maxCap)
